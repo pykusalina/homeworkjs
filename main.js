8bf5c39f64c367537bda3e608ec928698a076ea5
Alina Pykus <alinapykus17@email.com>
2022-07-31 15:52:15 +0300

    add js file

---
+++ main.js
@@ -0,0 +1,54 @@
+//task 2
+console.log('Pykus');
+
+//Task 3
+// let num = 1;
+// let string = 'hello';
+// alert(num, string);
+// num = string;
+
+
+//Task 4
+//  let cat = {
+//     name: 'richard',
+//     age: 2,
+//      hungry: true,
+//     happy: undefined,
+//     sad: null
+// };
+
+//Task 5
+// let isAdult = confirm('How old are you?');
+// alert(isAdult);
+
+//Task 6
+// let name = 'Alina';
+// let lastName = 'Pykus';
+// let group = 'js fundamentals';
+// let DOB = 2003;
+// let areMarried = false;
+// console.log(DOB, areMarried, name, lastName, group);
+//  let secondName = null;
+//  let nickname = undefined;
+//  console.log(secondName, nickname);
+
+
+//Task 7
+// let login = prompt('your login is User', 'User');
+// alert(login);
+
+// let email = prompt('your email is user@mail.com', 'user@mail.com');
+// alert(email);
+
+// let password = prompt('your password is 1111', '1111');
+// alert(password);
+
+//Task 8
+// let sec = 60;
+// alert(sec * sec);
+
+// let hours = 24;
+// alert(sec * sec * hours);
+
+// let month = 30;
+// alert(sec  * hours * month);
